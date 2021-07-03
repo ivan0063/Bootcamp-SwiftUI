24be2aae38699502631d31c3488ecbf970f5b2a4
J. Ivan Martinez Mateos <jimm0063@gmail.com>
2021-07-03 17:49:07 -0500

    Initial Commit

---
+++ BootcampSwiftUI/BootcampSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BootcampSwiftUIApp.swift
+//  BootcampSwiftUI
+//
+//  Created by J. Ivan Martinez Mateos on 03/07/21.
+//
+
+import SwiftUI
+
+@main
+struct BootcampSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BootcampSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BootcampSwiftUI
+//
+//  Created by J. Ivan Martinez Mateos on 03/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
